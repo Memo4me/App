03e0dfedb427e1bed459e0e3fc4d65332cd3d642
liwwesS <ka.novik@yandex.by>
2024-10-16 22:47:40 +0300

    1. Changed orm from sql server to postresql 2. Added an extension method 3. Fixed dockerfile

---
+++ src/Infrastructure/Memo4me.Persistence/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Memo4me.Persistence;
+
+public static class Extensions
+{
+    public static void AddPersistence(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(x =>
+        {
+            x.UseNpgsql("Username=postgres;Password=123;Host=db;Port=5432;Database=MemoDb");
+        });
+    }
+}
--- src/Presentation/Memo4me.API/Program.cs
+++ src/Presentation/Memo4me.API/Program.cs
@@ -3,41 +3,29 @@ using Memo4me.Application.Interfaces.Services;
 using Memo4me.Application.Services;
 using Memo4me.Persistence;
 using Memo4me.Persistence.Repositories;
-using Microsoft.EntityFrameworkCore;
 
-namespace Memo4me.API;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistence();
+        
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+var app = builder.Build();
+        
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        
-        builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-        
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<IAuthService, AuthService>();
-
-        var app = builder.Build();
-        
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+        
+app.MapControllers();
+
+app.Run();
